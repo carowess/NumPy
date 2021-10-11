ba501bd5a846f9a2066176c6f36459ac5c7e70ba
Caroline Wescott <caroline_wescott1@baylor.edu>
2021-10-11 17:15:23 -0500

    class work

--- MyArrays1.py
+++ MyArrays1.py
@@ -42,5 +42,57 @@ arr07 = np.arange(5,10)
 
 arr08 = np.arange(10,1,-2)
 
+arr09 = np.linspace(0.0,1.0,num=5)
+
+arr10 = np.arange(1,21).reshape(4,5) #makes 4 rows with 5 columns each
+
+
+num01 = np.arange(1,6)
+
+num02 = num01*2 #doesn't affect original array
+
+num03 = num01*3    #doesn't affect original array #broadcasting- takes the number and applies it to every number in the array
+
+num01 += 10 #DOES affect original array (it changed)
+
+num04 = num01 * num02 #must be the same size
+
+num05 = num01 > 13 #shows True or False
+
+num06 = num03 > num01 #shows True or False
+
+#Here we have an array of 4 students grades on 3 exams
+#row = student
+#col = exam
+grades = np.array([[87,96,70],[100,87,90],
+                    [94,77,90],[100,81,82]])
+
+print(grades.sum())
+print(grades.mean())
+print(grades.std())
+print(grades.var())
+
+# calculate average on all rows for each column
+grades_by_exam = grades.mean(axis=0) #one-dimensinal ray of 3 elements
+
+#calculate average on all cols for each row
+grades_by_student = grades.mean(axis=1) #one-dimensional ray for 4 values
+
+num07 = np.array([1,4,9,16,25,36])
+num08 = np.sqrt(num07)
+
+num09 = np.array([10,20,30,40,50,60])
+num10 = np.add(num07,num09)
+#same as
+num10 = num07 +num09
+
+num11 = np.multiply(num09,5)
+#same as
+num11 = num09 * 5 #broadcasting
+
+num12 = num09.reshape(2,3)
+num13 = np.array([2,4,6])
+
+num14 = np.multiply(num12,num13) #DIFFERENT SHAPES, # of rows DON'T have to match up, but number of columns DO
 
 print()
---
+++ MyArrays2.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+# ROWS - Each student (student0, student1, student2, student3)
+# COLS - Each Test (Test0, Test1, Test2)
+grades = np.array([[87,96,70],[100,87,90],
+                    [94,77,90],[100,81,82]])
+
+
+student1 = grades[1]
+
+student0_test1 = grades[0,1]
+
+students0_1 = grades[0:2] #colon represents sequential values, the UPPER BOUND is NOT included (only 0 and 1 are included in this case)
+
+students1and3 = grades[[1,3]] #represents the row part, not the column part, this gives us all the grades for students 1 and 3
+
+#to select students 1 and 3 but only test 2
+students1and3_test2 = grades[[1,3],2]
+
+all_students_test0 = grades[:,0]
+
+all_students_test1_2 = grades[:,1:3]
+
+all_students_test0and2 = grades[:,[0,2]]
+
+'''
+Use NumPy random-number generation to create an array of twelve 
+random grades in the range 60 through 100, then reshape the result 
+into a 3-by-4 array. Calculate the average of all the grades, 
+the averages of the grades for each test, and the averages of the 
+grades for each student
+'''
+
+arr_random = np.random.randint(60,101,12).reshape(3,4)
+
+grades.mean()
+
+#avg by col
+grades.mean(axis=0) #represents average by TEST SCORE (column)
+
+#avg by row
+grades.mean(axis=1) #represents average by STUDENT (row)
+
+
+numbers = np.arange(1,6)
+
+####SHALLOW COPY####
+numbers_view = numbers.view() #creating a SHALLOW copy
+
+numbers[1] *= 10 #copy is affected when the original is changed
+
+numbers_view[1] /= 10 #original is affected when the copy is changed
+
+numbers_slice = numbers[0:3] #shallow copy
+
+numbers[1] *= 20
+
+####DEEP COPIES####
+numbers_copy = numbers.copy()
+
+numbers[1] *= 10 #the copy is NOT changed when the original is changed
+
+grades = np.array([[87,96,70],[100,87,90]])
+
+#RESHAPE METHOD produces a SHALLOW COPY
+grades_reshaped = grades.reshape(1,6)
+
+grades.resize(1,6)
+
+#FLATTEN METHOD creates a DEEP copy
+flattened = grades.flatten()
+
+#RAVEL creats a shallow copy
+raveled = grades.ravel()
+
+raveled[0] = 100
+
+flattened[1] = 100
+
+grades = np.array([[87,96,70],[100,87,90]])
+
+grades.T #flips the array
+
+grades2 = np.array([[94,77,90],[100,81,82]])
+
+
+h_grades = np.hstack((grades,grades2)) ##adds more columns (exam)
+
+v_grades = np.vstack((grades,grades2)) ##adds more rows (students)
+
+print()
+
+
+
